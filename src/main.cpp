b08d4a18ada31b43deb26a50df3f42eddff6f5ab
vietdd <vietdaoduy@gmail.com>
2017-03-22 08:42:08 +0700

    xoa comment

--- src/main.cpp
+++ src/main.cpp
@@ -30,6 +30,5 @@ int main(int argc, char *argv[])
     //VehicleTrackingServer vehicleTrackingServer(QDateTime::fromString(QDateTime::currentDateTime().toString("yyyy-MM-dd"), "yyyy-MM-dd").addDays(-60), 0, 1235);
     VehicleTrackingServer vehicleTrackingServer;
 
-    //kjshfkjsdhkfhd
     return a.exec();
 }
